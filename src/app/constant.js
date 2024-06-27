714f31839315e0af03f867e2634b6ac6947663ee
Sahil Lede <sahillede@Sahils-MBP.guest.corp.microsoft.com>
2024-06-27 10:33:07 +0530

    fixed cors error

--- src/app/constant.js
+++ src/app/constant.js
@@ -1,7 +1,7 @@
-const PHI3_LOCATION = "local";
+const PHI3_LOCATION = "remote";
 // const PHI3_LOCATION = "remote";
 
 export const URL =
   PHI3_LOCATION === "local"
     ? "http://127.0.0.1:5000"
-    : "https://jj8bzvnc-5000.inc1.devtunnels.ms";
+    : "http://parent-bot.centralindia.cloudapp.azure.com:5000";
